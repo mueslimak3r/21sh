29052da1ebf079a6d2e8f09eed06557b153b9260
cameron <cameronlambert98@gmail.com>
2019-09-08 02:45:23 -0700

    testing stuff

--- src/ast.c
+++ src/ast.c
@@ -1,24 +1,25 @@
 #include "ftshell.h"
 
-
-
-void    make_tree(t_ast **root, char *line)
+int		make_tree(t_ast **root, char *line, int i)
 {
-    t_ast *new;
+    t_ast	*new;
+	t_token	*head;
+	int		ret;
+	int		op;
 
+	head = NULL;
+	ret = i;
     if (!*root)
     {
         *root = ft_memalloc(sizeof(t_ast));
         new = *root;
+		return(make_tree((*root)->));
     }
     else
     {
         new = ft_memalloc(sizeof(t_ast));
     }
-    while (line)
-    {
-        line += make_tree(&new->left, line)
-        line += make_tree(&new->right, line);
-    }
-    return (new);
+	ret += get_token(line, ret, &head);
+	ret += get_token(line, ret, &head);
+    return (i);
 }
